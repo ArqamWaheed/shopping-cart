d153d1ae960a12635b08cc9b9b52b353f34ea296
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2026-01-10 20:44:56 +0500

    Feat: Desktop version of homepage document

---
+++ src/Homepage.jsx
@@ -0,0 +1,20 @@
+import './Homepage.css'
+import wallpaper from './assets/Wallpaper.png' 
+
+function Homepage() {
+  return (
+    <>
+      <div className="Homepage">
+        <img className="WallpaperImg" src={wallpaper} alt="Wallpaper" /> 
+        <div className="textContent">
+          <h1>BROWSE THE <span>COLLECTION</span></h1>
+          <hr></hr>
+          <p>Authentic One Piece figurines crafted for collectors who value detail, quality, and character accuracy. Each piece captures the spirit of the character, from sculpting to paint, made to stand out on any shelf.</p>
+          <button class="BuyBtn">Buy Now</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Homepage
