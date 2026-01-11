766b37cd263daca33415626cf03ac1dd6d88dbfc
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2026-01-11 20:30:17 +0500

    Feat: Responsive HomePage

--- src/ui/components/NavigationBar.jsx
+++ src/ui/components/NavigationBar.jsx
@@ -1,11 +1,21 @@
 import "../styles/NavigationBar.css";
+import Logo from "../../assets/Logo.png"
 
 function NavigationBar() {
 
     return (
         <nav>
-            <div className="navLogoDiv"></div>
-            <div className="navNavDiv"></div>
+            <div className="navLogoDiv">
+                <img className="imgLogo" src={Logo} alt="Logo" />
+                <h2>One <span>P</span>iece</h2>
+            </div>
+            <div className="navNavDiv">
+                <p>Home</p>
+                <p>Shop</p>
+                <p>About</p>
+                <p>Contact</p>
+                <p>FAQ</p>
+            </div>
         </nav>
     )
 }
