e3f51d1e11b3942e8efd0543284f0f4d5259ccd1
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2026-01-12 23:11:59 +0500

    Feat: Responsive NavBar

--- src/ui/components/NavigationBar.jsx
+++ src/ui/components/NavigationBar.jsx
@@ -10,11 +10,12 @@ function NavigationBar() {
                 <h2>One <span>P</span>iece</h2>
             </div>
             <div className="navNavDiv">
-                <p>Home</p>
-                <p>Shop</p>
-                <p>About</p>
-                <p>Contact</p>
-                <p>FAQ</p>
+                <p className="nav-hide">Home</p>
+                <p className="nav-hide">Shop</p>
+                <p className="nav-hide">About</p>
+                <p className="nav-hide">Contact</p>
+                <p className="nav-hide">FAQ</p>
+                <svg class="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" aria-hidden="true"><path class="menu-svg" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>           
             </div>
         </nav>
     )
