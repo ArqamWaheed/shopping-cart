076d4843a31d936a3b9810cd012d38ee6fa38f4d
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2026-01-13 13:59:01 +0500

    Feat: Restructured the file architecture

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Homepage from './ui/components/Homepage.jsx'
-import NavigationBar from './ui/components/NavigationBar.jsx'
+import Homepage from '../ui/components/Homepage.jsx'
+import NavigationBar from '../ui/components/NavigationBar.jsx'
 
 
 createRoot(document.getElementById('root')).render(
