44e5f0d13f97e9ae96ce1e441c33d589c14498c0
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2026-01-13 15:12:08 +0500

    Feat: React Router Error Handling and some packages

--- src/ui/Routes/routes.jsx
+++ src/ui/Routes/routes.jsx
@@ -1,14 +1,20 @@
 import Homepage from "../pages/Homepage";
 import { MainLayout } from "../layouts/MainLayout";
+import ErrorPage from "../pages/Errorpage";
 
 const routes = [
     {
         path: "/",
         element: <MainLayout />,
+        errorElement: <ErrorPage />,
         children: [
             {
                 index: true,
                 element: <Homepage />,
+            },
+            {
+                path: "shop",
+                element: null,
             }
         ]
     }, 
--- src/ui/layouts/components/NavigationBar.jsx
+++ src/ui/layouts/components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import '../../../styles/NavigationBar.css';
 import Logo from '../../../assets/Logo.png'
+import { Link } from 'react-router';
 
 function NavigationBar() {
     return (
@@ -9,11 +10,11 @@ function NavigationBar() {
                 <h2>One <span>P</span>iece</h2>
             </div>
             <div className="navNavDiv">
-                <p className="nav-hide">Home</p>
-                <p className="nav-hide">Shop</p>
-                <p className="nav-hide">About</p>
-                <p className="nav-hide">Contact</p>
-                <p className="nav-hide">FAQ</p>
+                <p className="nav-hide"><Link to="/">Home</Link></p>
+                <p className="nav-hide"><Link to="/shop">Shop</Link></p>
+                <p className="nav-hide"><Link to="">About</Link></p>
+                <p className="nav-hide"><Link to="">Contact</Link></p>
+                <p className="nav-hide"><Link to="">FAQ</Link></p>
                 <svg className="menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30" aria-hidden="true"><path className="menu-svg" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>           
             </div>
         </nav>
