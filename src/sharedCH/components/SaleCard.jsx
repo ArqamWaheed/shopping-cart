5e7e869d1c99eae74bdd139f91f93c7b763e8724
ArqamWaheed <86839084+ArqamWaheed@users.noreply.github.com>
2026-01-14 19:31:07 +0500

    Feat: Added modular CSS for the card component

--- src/sharedCH/components/SaleCard.jsx
+++ src/sharedCH/components/SaleCard.jsx
@@ -1,12 +1,12 @@
-import "../styles/SaleCard.css";
+import styles from "../styles/SaleCard.module.css";
 
 function SaleCard({imgSrc, figurineName, figurinePrice}) {
     return (
-        <div className="saleCard">
-            <img className="saleCardImg" src={imgSrc} alt="FigurineImage" />
+        <div className={styles.saleCard}>
+            <img className={styles.saleCardImg} src={imgSrc} alt={figurineName} />
             <h2>{figurineName}</h2>
             <h3>{figurinePrice}</h3>
-            <button className="saleCardBtn">
+            <button className={styles.saleCardBtn}>
                 Add To Cart
             </button>
         </div>
